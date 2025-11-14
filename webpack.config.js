064372dbbdc5503a4bb83192c5ae59b0d60da186
Matej Samler <matej.samler@thenetw.org>
2025-11-14 12:01:47 +0100

    Fix publicPath in webpack config for consistent asset loading

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: process.env.NODE_ENV === 'production' ? '/Metju.Chord/' : '/',
+        publicPath: '/',
     },
     mode: 'development',
     resolve: {
